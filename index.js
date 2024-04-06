96fae296ba8bd2c2a704bfe9fd59f6a63bb98154
Alice Veselá <alice.hladikova@gmail.com>
2024-04-06 14:11:52 +0200

    rozsviceni zarovky

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('funguju!');
+const bulb = document.querySelector('.bulb');
+
+/*const lightOn = () => {
+  bulb.classList.toggle('bulb--on');
+};
+document.addEventListener('keydown', lightOn);
+*/
+
+document.addEventListener('keydown', () => {
+  bulb.classList.toggle('bulb--on');
+});
